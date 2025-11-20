89de3e7d4b31152803301e4dd49348ddaa6633e7
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-20 18:42:44 -0300

    Update logic.h

--- EnzoOs/Drivers/accessibility/braille/logic.h
+++ EnzoOs/Drivers/accessibility/braille/logic.h
@@ -1,2 +1,6 @@
-#define linux_enzoos_braile_letter "VALUE"
-#define linux_enzoos_braile_number "VALUE"
+#pragma once
+
+#include <linux/printk.h>
+
+#define linux_enzoos_braile_letter(x) printk("Letra braille registrada: %s\n", x)
+#define linux_enzoos_braile_number(x) printk("Número braille registrado: %s\n", x)
