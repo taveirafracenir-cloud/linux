9c8e100b186dacca836fd7ad3fb640e494310eeb
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-20 12:12:25 -0300

    Create __define_ui_defines.c

---
+++ EnzoOs/ui/__define_ui_defines.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#    define __ui_label_text
+#    define __ui_text
+#    define __ui_button
+#    define __ui_button_onclick
+#    define __ui_ester_egg_xml_ui
+#    define __ui_colors
+#    define __ui_RGB
+#    define __ui_graphics
+#    define __ui_linux_kernel_button_onclick
+#    define __ui_OpenGL
+#    define __ui_OpenCL
+#    define __ui_widget_manager
+#    define __ui_app_stop
+#    define __ui_R
+#    define __ui_G
+#    define __ui_B
+#    define __ui_mouse_input
+#    define __ui_keyboard_input
+#    define __ui_touchscreen_input
+#    define __ui_xml_ui
+#    define __ui_alert
+#    define __ui_notification
+#    define __ui_emergence_alert
+#    define __ui_microphone
+#    define __ui_colors
+#    define __ui_view_apps
+/**
+ {APP_ICON}
+  {=======}
+  APP_NAME
+  */
+#    define __ui_emoji
+#    define __ui_utf_8
+#    define __ui_police_alert_mode
+#    define __ui_sos_mode /** modo de emergência */
+#    define __ui_factory_mode
+#    define __ui_loanding_bar_frames
+#    define __ui_fuse_time
+#    define __ui_xml_leitor
