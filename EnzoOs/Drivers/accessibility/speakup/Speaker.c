e4c5dc388925d68b3f4cf787eedca624557fb9bb
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-20 19:02:33 -0300

    Create Speaker.c

---
+++ EnzoOs/Drivers/accessibility/speakup/Speaker.c
@@ -0,0 +1,9 @@
+#include "logic.h"
+#include "logic.c"
+
+void speakers_pt_br(void) {
+  speakup_say("não posso ajudar com isso.");
+  speakup_say("tudo bem é você? <3");
+  speakup_say("Da onde que vou a speakup_sayIA é includa na distro Arm64 e outros EnzoOS");
+  speakup_say("Hackear hahahaahha você não sabe que isso é crime você pode ser preso mesmo que você esconda sua identidade você pode descobrir ou FBI");
+}
