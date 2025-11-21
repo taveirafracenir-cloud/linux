a182c6547f1ef2b5df7a42a7e1336c81cf66f739
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-11-20 22:11:24 -0300

    Create fuse_time_brazil.c

---
+++ EnzoOs/FUSE_TIMES/BRAZIL/fuse_time_brazil.c
@@ -0,0 +1,35 @@
+#ifndef FUSE_TIME_BRAZIL_H
+#define FUSE_TIME_BRAZIL_H
+
+#define FUSE_TIME_BRAZIL_FERNANDO_DE_NORONHA   "-02:00"
+#define FUSE_TIME_BRAZIL_BRASILIA              "-03:00"
+#define FUSE_TIME_BRAZIL_SAO_PAULO             "-03:00"
+#define FUSE_TIME_BRAZIL_RIO_DE_JANEIRO        "-03:00"
+#define FUSE_TIME_BRAZIL_MINAS_GERAIS          "-03:00"
+#define FUSE_TIME_BRAZIL_BAHIA                 "-03:00"
+#define FUSE_TIME_BRAZIL_CEARA                 "-03:00"
+#define FUSE_TIME_BRAZIL_PERNAMBUCO            "-03:00"
+#define FUSE_TIME_BRAZIL_RIO_GRANDE_DO_SUL     "-03:00"
+#define FUSE_TIME_BRAZIL_PARANA                "-03:00"
+#define FUSE_TIME_BRAZIL_SANTA_CATARINA        "-03:00"
+#define FUSE_TIME_BRAZIL_GOIAS                 "-03:00"
+#define FUSE_TIME_BRAZIL_DISTRITO_FEDERAL      "-03:00"
+#define FUSE_TIME_BRAZIL_MARANHAO              "-03:00"
+#define FUSE_TIME_BRAZIL_PARAIBA               "-03:00"
+#define FUSE_TIME_BRAZIL_SERGIPE               "-03:00"
+#define FUSE_TIME_BRAZIL_PIAUI                 "-03:00"
+#define FUSE_TIME_BRAZIL_RIO_GRANDE_DO_NORTE   "-03:00"
+#define FUSE_TIME_BRAZIL_ESPIRITO_SANTO        "-03:00"
+#define FUSE_TIME_BRAZIL_PARA                  "-03:00"
+#define FUSE_TIME_BRAZIL_AMAPA                 "-03:00"
+#define FUSE_TIME_BRAZIL_TOCANTINS             "-03:00"
+
+#define FUSE_TIME_BRAZIL_MATO_GROSSO           "-04:00"
+#define FUSE_TIME_BRAZIL_MATO_GROSSO_DO_SUL    "-04:00"
+#define FUSE_TIME_BRAZIL_RONDONIA              "-04:00"
+#define FUSE_TIME_BRAZIL_RORAIMA               "-04:00"
+#define FUSE_TIME_BRAZIL_AMAZONAS              "-04:00"
+
+#define FUSE_TIME_BRAZIL_ACRE                  "-05:00"
+
+#endif
